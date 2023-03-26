c3e47523bc7507805fcb806c6f653fab97f3a6bd
Harold Dinglasan <ac153990@avcol.school.nz>
2023-03-26 18:21:41 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MaoriAssessment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The game displays a menu of difficulty levels to choose from, and the player selects one. After selecting a difficulty level, the game clears the console and displays instructions for the quiz.
+
+              The quiz consists of 5 multiple-choice questions, with each correct answer adding one point to the player's score. The game displays the player's score and the number of questions remaining after each question. The game loops through each question using a for loop with an iteration of 1.
+
+             Each question is an element in an array of strings, with the answer choices presented to the player using a switch statement. The code also includes validation for invalid input, requiring the player to select a valid answer. */
+
+
+
+            Console.WriteLine("Welcome to the Maori Quiz! In this quiz you will get to choose between three difficulties easy, medium, and hard.");
+            Console.WriteLine()
+        }
+    }
+}
