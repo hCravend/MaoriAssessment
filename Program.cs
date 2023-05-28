8f8746d972c30be7d62c1007dd2031fb7c539c99
Harold Dinglasan <ac153990@avcol.school.nz>
2023-05-28 15:09:40 +1200

    removed restart method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+using System.Net.Http.Headers;
 
 namespace MaoriAssessment
 {
@@ -6,7 +9,7 @@ namespace MaoriAssessment
     {
         static void Main(string[] args)
         {//start of main method
-            
+
             /*The game displays a menu of difficulty levels to choose from, and the player selects one. After selecting a difficulty level, the game clears the console and displays instructions for the quiz.
 
               The quiz consists of 5 multiple-choice questions, with each correct answer adding one point to the player's score. The game displays the player's score and the number of questions remaining after each question. The game loops through each question using a for loop with an iteration of 1.
@@ -25,8 +28,8 @@ namespace MaoriAssessment
 
         static void start()
         {//start of start method
-            
-            
+
+
             Console.WriteLine("Welcome to the Maori Quiz!"); //greets the user
             Console.Write("Please input your name: "); //asks the user to input their name
             string name = Console.ReadLine();
@@ -36,29 +39,29 @@ namespace MaoriAssessment
                 Console.Write("\nNopeee you need to enter your name ._.:");
                 name = Console.ReadLine();
             }//end of while statement
-            
+
             Console.WriteLine($"\nHello {name}! Welcome to the Maori Quiz!"); //displays the name
             Console.WriteLine("\n\t\t\t------PRESS ANY KEY TO CONTINUE------");
             Console.ReadKey();
             Console.Clear();
             menu(name);
 
-            
+
 
         }//end of start method
 
         static void menu(string name)
         {//start of menu method
-            
+
             string difficulty; //used string instead of int so when the user enters a number or nothing it will still display the error message
-            
+
             Console.WriteLine($"Please choose a difficulty {name}:");
             Console.WriteLine("1. Easy\n2. Medium\n3. Hard");
             Console.Write("Please enter a number 1-3: ");
             difficulty = Console.ReadLine();
 
             //while loop validating the choices, if the user doesnt enter between (1-3) it will display the error message
-            
+
             while (difficulty != "1" && difficulty != "2" && difficulty != "3")
             {
                 Console.Write($"\nInvalid input, {difficulty} is  not a valid choice. Please enter a number between 1-3: ");
@@ -67,7 +70,7 @@ namespace MaoriAssessment
 
 
             //The switch statement is selecting a function to execute based on the value of the "difficulty" variable.
-             switch (difficulty)
+            switch (difficulty)
             {
                 case "1":
                     Easy(name);
@@ -85,12 +88,7 @@ namespace MaoriAssessment
         static void Easy(string name)
         {//start of easy method
 
-            Console.Clear();// Clears the menu after the user selects a difficulty and displays the instructions
-            Console.WriteLine("This is a 5 question multiple choice quiz. Please Choose an answer between A-D.");
-            Console.WriteLine("Your score and the ammount of questions you have left will be displayed on the top left side of the test.");
-            Console.WriteLine("\n\t\t\t\t\t-----PRESS ANY KEY TO CONTINUE-----");
-            Console.ReadKey();//makes the user press a key before continuing the test
-            Console.Clear();
+            introduction();
 
             //stores all of the easy questions
             string[] EasyQ = new string[5];
@@ -107,15 +105,14 @@ namespace MaoriAssessment
             EasyA[2] = "B";
             EasyA[3] = "A";
             EasyA[4] = "C";
-            
+
             string user_ans;
             int score = 0;
-            int Qnum = 1;
 
             for (int i = 0; i < 5; i++)//loops through all of the questions by 5
             {//start of for loop
                 Console.WriteLine($"Score:{score}");//displays the score
-                Console.WriteLine($"Question {Qnum}");// displays the question number 
+                Console.WriteLine($"Question {i + 1}");// displays the question number 
                 Console.WriteLine(EasyQ[i]);//displays the question and choices
                 Console.Write("Please choose an answer: ");
                 user_ans = Console.ReadLine().ToUpper();
@@ -143,57 +140,19 @@ namespace MaoriAssessment
 
                 Console.WriteLine("\n\t\t\t\t------PRESS ANY KEY TO CONTINUE------");
                 Console.ReadKey();
-                Qnum++;
                 Console.Clear();
             }//end of for loop
 
+            pass_fail(score, name);
+            
+            restart(name);
 
-
-            //if statement determining the passing score for the quiz
-            if (score <= 6)
-            {
-                Console.WriteLine($"Good effort {name}! You scored {score}/10, so close!");//if the user gets 6 below they fail
-            }
-            else if (score >= 8)
-            {
-                Console.WriteLine($"Amazing job {name}! You scored {score}/10, fantastic! ");//if the user gets anything higher they pass
-            }
-
-           Console.Write("\nWould you like to play again? (Y/N): ");
-            string retry = Console.ReadLine().ToUpper();
-
-            //while loop that validates the user's input for (Y/N)
-            while (retry != "Y" && retry != "N") 
-            {
-                Console.Write($"\n{retry} is not a valid choice. Please choose between (Y/N): ");
-                retry = Console.ReadLine().ToUpper();
-            }
-
-            switch(retry)
-            {
-                //if the user chooses "Y" it will return to the menu method and repeat the whole quiz
-                case "Y": 
-                    Console.Clear();
-                    menu(name);
-                    break;
-                //if the user chooses "N" a goodbye message will appear and will thank the use for playing  and end the quiz
-                case "N":
-                    Console.WriteLine($"\nGoodbye {name}! Thank you for playing!");
-                    break;
-            }
-        
-        
         }//end of easy method
 
         static void Medium(string name)
         {//start of medium method
 
-            Console.Clear();// Clears the menu after the user selects a difficulty and displays the instructions
-            Console.WriteLine("This is a 5 question multiple choice quiz. Please Choose an answer between A-D.");
-            Console.WriteLine("Your score and the ammount of questions you have left will be displayed on the top left side of the test.");
-            Console.WriteLine("\n\t\t\t\t\t-----PRESS ANY KEY TO CONTINUE-----");
-            Console.ReadKey();//makes the user press a key before continuing the test
-            Console.Clear();
+            introduction();
 
             //stores all of the medium questions
             string[] MedQ = new string[5];
@@ -212,12 +171,11 @@ namespace MaoriAssessment
 
             string user_ans;
             int score = 0;
-            int Qnum = 1;
 
-            for (int i  = 0; i < 5; i++)//loops through all the questions by 5
+            for (int i = 0; i < 5; i++)//loops through all the questions by 5
             {//start of for loop
                 Console.WriteLine($"Score:{score}");//displays the score
-                Console.WriteLine($"Question {Qnum}");// displays the question number
+                Console.WriteLine($"Question {i + 1}");// displays the question number
                 Console.WriteLine(MedQ[i]);//displays the question and choices
                 Console.Write("Please choose an answer: ");
                 user_ans = Console.ReadLine().ToUpper();
@@ -238,67 +196,31 @@ namespace MaoriAssessment
                 }
                 else if (user_ans != MedA[i])
                 {
-                   //if the answer is wrong it will display this message
+                    //if the answer is wrong it will display this message
                     Console.WriteLine($"\nNice try {name}! The correct answer was {MedA[i]}");
 
                 }
 
                 Console.WriteLine("\n\t\t\t\t------PRESS ANY KEY TO CONTINUE------");
                 Console.ReadKey();
-                Qnum++;
                 Console.Clear();
             }//end of for loop
 
 
-            
-            //if statement determining the passing score for the quiz
-            if (score <= 6)
-            {
-                Console.WriteLine($"Good effort {name}! You scored {score}/10, so close!");//if the user gets 6 below they fail
-            }
-            else if (score >= 8)
-            {
-                Console.WriteLine($"Amazing job {name}! You scored {score}/10, fantastic! ");//if the user gets anything higher they pass
-            }
 
-            Console.Write("\nWould you like to try again? (Y/N): ");
-            string retry = Console.ReadLine().ToUpper();
+            pass_fail(score, name);
 
-            
-            //while loop that validates the user's input for (Y/N)
-            while (retry != "Y" && retry != "N")
-            {
-                Console.Write($"\n{retry} is not a valid choice. Please choose between (Y/N): ");
-                retry = Console.ReadLine().ToUpper();
-            }
-
-            switch (retry)
-            {
-                //if the user chooses "Y" it will return to the menu method and repeat the whole quiz
-                case "Y":
-                    Console.Clear();
-                    menu(name);
-                    break;
-                //if the user chooses "N" a goodbye message will appear and will thank the user for playing and end the quiz.
-                case "N":
-                    Console.WriteLine($"\nGoodbye {name}! Thank you for playing!");
-                    break;
-            }
+            restart(name);
 
 
 
         }//end of medium method
-            
+
 
         static void Hard(string name)
         {//start of hard method
-           
-            Console.Clear();// Clears the menu after the user selects a difficulty and displays the instructions
-            Console.WriteLine("This is a 5 question multiple choice quiz. Please Choose an answer between A-D.");
-            Console.WriteLine("Your score and the ammount of questions you have left will be displayed on the top left side of the test.");
-            Console.WriteLine("\n\t\t\t\t\t-----PRESS ANY KEY TO CONTINUE-----");
-            Console.ReadKey();//makes the user press a key before continuing the test
-            Console.Clear();
+
+            introduction();
 
             //stores all of the hard questions in an array
             string[] HardQ = new string[5];
@@ -318,13 +240,13 @@ namespace MaoriAssessment
 
             int score = 0;
             string user_ans;
-           
 
 
-            for(int i = 0; i < 5; i++)//loops through all of the questions by 5
+
+            for (int i = 0; i < 5; i++)//loops through all of the questions by 5
             {//start of for loop
                 Console.WriteLine($"Score:{score}");//displays the score
-                Console.WriteLine($"Question {i++}");//displays the question number
+                Console.WriteLine($"Question {i + 1}");//displays the question number
                 Console.WriteLine(HardQ[i]);//displays the quesstion and choices
                 Console.Write("Please choose an answer: ");
                 user_ans = Console.ReadLine().ToUpper();
@@ -341,13 +263,13 @@ namespace MaoriAssessment
                     //if the answer is correct it will display this message
                     Console.WriteLine($"\nGood job {name}! That was the correct answer <3");
                     score = score + 2;
-                    
+
                 }
                 else if (user_ans != HardA[i])
                 {
                     //if the answer is wrong it will display this message
                     Console.WriteLine($"\nNice try {name}! The correct answer was {HardA[i]}");
-                    
+
                 }
 
                 Console.WriteLine("\n\t\t\t\t------PRESS ANY KEY TO CONTINUE------");
@@ -356,7 +278,27 @@ namespace MaoriAssessment
             }//end of for loop
 
 
+            pass_fail(score ,name);
 
+
+            restart(name);
+
+
+
+        }//end of hard method
+
+        static void introduction()
+        {//start of introduction method 
+            Console.Clear();// Clears the menu after the user selects a difficulty and displays the instructions
+            Console.WriteLine("This is a 5 question multiple choice quiz. Please Choose an answer between A-D.");
+            Console.WriteLine("Your score and the ammount of questions you have left will be displayed on the top left side of the test.");
+            Console.WriteLine("\n\t\t\t\t\t-----PRESS ANY KEY TO CONTINUE-----");
+            Console.ReadKey();//makes the user press a key before continuing the test
+            Console.Clear();
+        }//end of introduction method
+
+        static void pass_fail(int score,string name)
+        {//start of pass_fail method
             //if statement determining the passing score for the quiz
             if (score <= 6)
             {//start of if
@@ -365,34 +307,14 @@ namespace MaoriAssessment
             else if (score >= 8)
             {//start of else if
                 Console.WriteLine($"Amazing job {name}! You scored {score}/10, fantastic! ");//if the user gets anything higher they pass
-            }//end of else if 
+            }//end of else if
+        }//end of pass_fail method
 
-
-            Console.Write("\nWould you like to try again? (Y/N): ");
-            string retry = Console.ReadLine().ToUpper();
-
-            //while loop that validates the user's input for (Y/N)
-            while (retry != "Y" && retry != "N")
-            {//start of while loop
-                Console.Write($"\n{retry} is not a valid choice. Please choose between (Y/N): ");
-                retry = Console.ReadLine().ToUpper();
-            }//end of while loop
-
-            switch (retry)
-            {//start of switch loop
-                //if the user chooses "Y" it will return to the menu method and repeat the whole quiz
-                case "Y":
-                    Console.Clear();
-                    menu(name);
-                    break;
-                //if the user chooses "N" a goodbye message will appear and will thank the user for playing and end the quiz.
-                case "N":
-                    Console.WriteLine($"\nGoodbye {name}! Thank you for playing!");
-                    break;
-            }//end of switch loop
+        
 
 
 
-        }//end of hard method
+
+
     }
 } 
